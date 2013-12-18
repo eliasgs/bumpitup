6ed999a0c9dea1c1755a539772250ffd7ef8b468
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-12-18 23:53:09 +0100

    Make sure git is working in the right directory

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var ver = require('semver');
 var fs = require('fs');
 var exec = require('child_process').exec;
+var path = require('path');
 
-module.exports = function bump(path, release) {
-  var pkg = require(path);
+module.exports = function bump(file, release) {
+  var pkg = require(file);
   var version = pkg.version;
   release = release || 'patch';
 
   pkg.version = ver.inc(version, release);
   if (pkg.version) {
-    fs.writeFileSync(path, JSON.stringify(pkg, null, 2));
+    fs.writeFileSync(file, JSON.stringify(pkg, null, 2));
     console.log('version increased from %s to %s',
                 '\u001b[31m' + version + '\u001b[39m', 
                 '\u001b[32m' + pkg.version + '\u001b[39m');
-    var git = exec('git commit -am "v'+pkg.version+'"');
+    var git = exec('git commit -am "v'+pkg.version+'"',
+                   {cwd: path.dirname(file)});
     git.stdout.pipe(process.stdout);
     git.stderr.pipe(process.stderr);
     git.on('exit', function (code) {process.exit(code);});
