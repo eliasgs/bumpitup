03c395a3a31e98de755c23bae807ab688be1e7fd
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-12-19 00:17:42 +0100

    Refactor to a callback based api

--- bin/bump.js
+++ bin/bump.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
 var bump = require('../index');
-bump(process.env.PWD + '/package.json', process.argv[2]);
+var file = process.env.PWD + '/package.json';
+var release = process.argv[2];
+bump(file, release, function (err, stdout, stderr) {
+  if (stdout) console.log(stdout);
+  if (stderr) {
+    console.error(stderr);
+    process.exit(err.code);
+  } else if (err) {
+    throw err;
+  }
+});
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var path = require('path');
 
-module.exports = function bump(file, release) {
+module.exports = function bump(file, release, callback) {
   var pkg = require(file);
   var version = pkg.version;
   release = release || 'patch';
@@ -14,13 +14,9 @@ module.exports = function bump(file, release) {
     console.log('version increased from %s to %s',
                 '\u001b[31m' + version + '\u001b[39m', 
                 '\u001b[32m' + pkg.version + '\u001b[39m');
-    var git = exec('git commit -am "v'+pkg.version+'"',
-                   {cwd: path.dirname(file)});
-    git.stdout.pipe(process.stdout);
-    git.stderr.pipe(process.stderr);
-    git.on('exit', function (code) {process.exit(code);});
+    exec('git commit -am "v'+pkg.version+'"',
+         {cwd: path.dirname(file)}, callback);
   } else {
-    console.error('could not increase version: %s', pkg.version);
-    process.exit(1);
+    callback(new Error('could not increase version: ' + pkg.version));
   }
 }
