8235aa43fd4b66fa70bfa70d142c89a84f796be9
Elias G. Schneevoigt <eliasgs@gmail.com>
2014-01-15 12:28:44 +0100

    Make it possible to get current version of bump

--- bin/bump.js
+++ bin/bump.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
+var pkg = require(__dirname + '/../package.json');
 var bump = require('../index');
 var file = process.env.PWD + '/package.json';
-var release = process.argv[2];
-bump(file, release, function (err, stdout, stderr) {
+var arg = process.argv[2];
+if (arg == '--version' || arg == '-v') {
+  console.log(pkg.version);
+  process.exit(0);
+}
+
+bump(file, arg, function (err, stdout, stderr) {
   if (stdout) console.log(stdout);
   if (stderr) {
     console.error(stderr);
